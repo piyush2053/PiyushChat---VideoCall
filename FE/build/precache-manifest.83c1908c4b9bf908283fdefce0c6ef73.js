2a726e896e634de397ca28ef9a5d192edc25956c
piyush.patel <piyush.patel@rws.com>
2023-04-19 18:55:24 +0530

    Firease FIles

---
+++ FE/build/precache-manifest.83c1908c4b9bf908283fdefce0c6ef73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b7b88c33731fd3b443a519e4855b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72506336815fd1f8608a",
+    "url": "/static/css/main.08066f86.chunk.css"
+  },
+  {
+    "revision": "88811616e611738e898e",
+    "url": "/static/js/2.9d5712ef.chunk.js"
+  },
+  {
+    "revision": "896ada494114fc01fe3319876c0d961e",
+    "url": "/static/js/2.9d5712ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72506336815fd1f8608a",
+    "url": "/static/js/main.03ad4e07.chunk.js"
+  },
+  {
+    "revision": "83599bc8eba0244969ab",
+    "url": "/static/js/runtime-main.78c226cd.js"
+  }
+]);
